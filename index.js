1ab66880da6d693b9e4fefd421b3310cf9c74925
Rob Ede <robjtede@icloud.com>
2018-03-14 01:46:03 +0000

    add filtering

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 const todoItem = Vue.extend({
   props: ['todo'],
+  data: {
+    checked: 'todo.done',
+  },
   template: `
     <div>
       <label>
         {{ todo.text }}
-        <input type="checkbox">
+        <input type="checkbox" :checked="todo.done" @click="markAs">
       </label>
     </div>
-  `
+  `,
+  methods: {
+    markAs: function (event) {
+      this.todo.done = event.target.checked
+    }
+  }
 })
 
 const todoList = Vue.extend({
-  props: ['todos'],
+props: ['todos'],
   template: `
     <div>
       <todo-item
@@ -30,10 +38,24 @@ const app = new Vue({
   el: '#app',
   data: {
     title: 'Front-end Showdown',
+    filter: 'none',
     todos: [
       {id: 1, text: 'explain vue', done: true},
       {id: 2, text: 'write todo app', done: false},
       {id: 3, text: 'go to cavendish', done: false}
     ]
-  }
+  },
+  computed: {
+    filteredTodos: function () {
+      if (this.filter === 'complete') {
+        return this.todos.filter(x => x.done);
+      } else if (this.filter === 'incomplete') {
+        return this.todos.filter(x => !x.done);
+      } else {
+        // no filter
+        return this.todos;
+      }
+    }
+  },
+  methods: {}
 })
